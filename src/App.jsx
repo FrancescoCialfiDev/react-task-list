fa5275c47891c582e43ecfe243796c0a7155d5eb
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-12-11 18:27:10 +0100

    create first list

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
 // Importazione stile css Default
 import './App.css'
 // Importiamo l'array dal file tasks.js situato all'interno della cartella data
-import  tasks  from "../data/tasks"
+import tasks from "../data/tasks"
 
 
 function App() {
- // Logica Funzionamento
+    // Logica Funzionamento
+    const taskIncomplete = tasks.filter((element) => {
+        return element.state.includes("in_progress") || element.state.includes("backlog")
+    })
+    const taskComplete = tasks.filter((element) => {
+        return element.state.includes("completed")
+    })
+    console.log(taskIncomplete);
+    console.log(taskComplete);
 
-const arrayFiltred = tasks.filter((element) => {
- return  element.state.includes("in_progress") || element.state.includes("backlog")
-})
-console.log(arrayFiltred);
+    const incomplete = taskIncomplete.map((element) => {
+        return <li key={element.id} className='list-unstyled'>
+            <h1>{element.title}</h1>
+            <p>{`Priority: ${element.priority}`}</p>
+            <p>{`Est. Time: ${element.estimatedTime}`}</p>
+        </li>
+    })
 
+    // Sviluppo html 
+    return (
+        <>
+            <h1>Hello World!</h1>
 
+            <h2>Task da fare</h2>
 
-
-
-
- return (
-<ul></ul>
- )
-
+            <ul>
+                {incomplete}
+            </ul>
+        </>
+    )
 }
+
+
 
 export default App;
